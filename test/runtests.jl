76f8fcd465ac0d242bb44cce176cc58b65d38f99
kdomino <kdomino@iitis.pl>
2018-10-29 11:14:49 +0100

    further tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -123,8 +123,11 @@ end
 @testset "detectors" begin
   Random.seed!(42)
   x = vcat(rand(8,2), 5*rand(1,2), 30*rand(1,2))
+  x1 = vcat(rand(20,3), 5*rand(1,3), 30*rand(1,3))
   @test rxdetect(x, 0.9) == [false, false, false, false, false, false, false, false, false, true]
   @test hosvdc4detect(x, 4., 2; b=2) == [false, false, false, false, false, false, false, false, true, true]
+  @test hosvdc4detect(x1, 3.9, 1; b=1) == vcat(fill(false, 20), [true, true])
+  @test rxdetect(x1, 0.9) == vcat(fill(false, 20), [true, true])
   ls = fill(true, 10)
   ls1 = [true, true, true, true, true, true, true, true, false, false]
   c = cumulants(x, 4)
