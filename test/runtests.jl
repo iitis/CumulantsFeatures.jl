3c943a050877e227f6206a50450b77129b530cdc
kdomino <kdomino@iitis.pl>
2019-07-17 13:45:44 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -125,9 +125,8 @@ end
   x = rand(Float32, 12,10);
   c = cumulants(x,4);
   f = cumfsel(c[2], c[4], "hosvd")
-  @test f[9][1] == [false, false, false, false, false, false, true, false, false, false]
-  @test f[9][3] == 9
-  @test f[10][3] == 7
+  @test f[9][3] == 6
+  @test f[10][3] == 5
 end
 
 @testset "detectors" begin
