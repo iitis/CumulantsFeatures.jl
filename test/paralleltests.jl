8d459ed1b496e9ed832f175ccf583c43fdf24990
Krzysztof Domino <kdomino@iitis.pl>
2022-01-27 16:38:18 +0100

    correct testing of parallel implementation

--- test/paralleltests.jl
+++ test/paralleltests.jl
@@ -9,15 +9,28 @@ println(nworkers())
 
 @testset "greedesearch parallel implementation" begin
   g = greedesearchdata(a,b, testf, 3)
-  @test g[1][1] == [true, false, true]
-  @test g[2][1] == [false, false, true]
-  @test g[3][1] == [false, false, false]
-  @test g[1][2] ≈ 0.48918301293211774
-  @test g[2][2] ≈ 0.9735659798036858
-  @test g[3][2] == 1.0
-  @test g[1][3] == 2
-  @test g[2][3] == 1
-  @test g[3][3] == 3
+
+  if VERSION <= v"1.7"
+    @test g[1][1] == [true, false, true]
+    @test g[2][1] == [false, false, true]
+    @test g[3][1] == [false, false, false]
+    @test g[1][2] ≈ 0.48918301293211774
+    @test g[2][2] ≈ 0.9735659798036858
+    @test g[3][2] == 1.0
+    @test g[1][3] == 2
+    @test g[2][3] == 1
+    @test g[3][3] == 3
+  else
+    @test g[1][1] == [true, true, false]
+    @test g[2][1] == [true, false, false]
+    @test g[3][1] == [false, false, false]
+    @test g[1][2] ≈ 0.09764869605558585
+    @test g[2][2] ≈ 0.6293451231426089
+    @test g[3][2] == 1.0
+    @test g[1][3] == 3
+    @test g[2][3] == 2
+    @test g[3][3] == 1
+  end
 end
 
 Random.seed!(42)
